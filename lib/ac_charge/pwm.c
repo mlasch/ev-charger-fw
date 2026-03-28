9028d7dc0ee9173b11b14068ddfc3dc9a98231a5
Marc Lasch <mlasch@mailbox.org>
2026-03-28 12:24:58 +0100

    lib: pwm: Fix inverted CP PWM signal

--- lib/ac_charge/pwm.c
+++ lib/ac_charge/pwm.c
@@ -10,7 +10,7 @@ LOG_MODULE_REGISTER(pwm, CONFIG_AC_CHARGE_LOG_LEVEL);
 
 #include <lib/ac_charge.h>
 
-static const struct pwm_dt_spec pwm_out = PWM_DT_SPEC_GET(DT_ALIAS(pwm_out));
+static const struct pwm_dt_spec cp_pwm = PWM_DT_SPEC_GET(DT_ALIAS(cp_pwm));
 
 #define PWM_PERIOD_NS 1000000U /* 1 ms = 1 kHz */
 
@@ -22,7 +22,7 @@ int set_duty_cycle_percent(uint8_t percent)
 
     uint32_t pulse_ns = (PWM_PERIOD_NS * percent) / 100U;
 
-    int ret = pwm_set_dt(&pwm_out, PWM_PERIOD_NS, pulse_ns);
+    int ret = pwm_set_dt(&cp_pwm, PWM_PERIOD_NS, pulse_ns);
     if (ret < 0) {
         LOG_ERR("Failed to set PWM: %d", ret);
     }
@@ -31,13 +31,13 @@ int set_duty_cycle_percent(uint8_t percent)
 
 int init_ac_charge(void)
 {
-    if (!pwm_is_ready_dt(&pwm_out)) {
+    if (!pwm_is_ready_dt(&cp_pwm)) {
         LOG_ERR("PWM device not ready");
         return -ENODEV;
     }
 
     LOG_INF("Starting 1 kHz PWM at 50%% duty cycle");
-    int ret = set_duty_cycle_percent(60);
+    int ret = set_duty_cycle_percent(50);
     if (ret < 0) {
         return ret;
     }
